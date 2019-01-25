9c23f12345d5d6845358604fbfc6f9a08cc867f0
Hitesh Mor <hmor@srtekbox.com>
2019-01-25 16:33:24 +0530

    commit 3-1-2

--- GithubCollobrationPullRequest/ViewController.swift
+++ GithubCollobrationPullRequest/ViewController.swift
@@ -13,10 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        print("Line 1 ")
+        // modified all three files
+        print("Line 3 ")
+        print("Line 1")
         print("Line 2")
-        print("Line 3")
     }
 
 
