3b88eb5a9b8406b2fb28c1a1f639762f45d0685b
Hitesh Mor <hmor@srtekbox.com>
2019-01-25 15:31:53 +0530

    add both changes conflicts

--- GithubCollobrationPullRequest/ViewController.swift
+++ GithubCollobrationPullRequest/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        print("Hitesh add I wanna chang same line ")
+        print("Hitesh add I wanna chang same line ")
+        
         print("Hitesh add this in his personnelbranch")
     }
 
